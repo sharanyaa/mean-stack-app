c95ffa359451f267bb143658664289ce437c3b8b
sharanya <sharanya.517@gmail.com>
2017-03-08 22:41:26 -0800

    inserts and queries using the node.js driver

--- index.js
+++ index.js
@@ -26,7 +26,30 @@ mongodb.MongoClient.connect(url, function(error, db) { // callback
 			docs.forEach(function(doc) {
 				console.log(JSON.stringify(doc));
 			});
-		process.exit(0);
+		});
+		var doc = {
+			title: 'Jaws',
+			year: 1975,
+			director: 'Steven Speilberg',
+			rating : 'PG'
+		};
+		db.collection('movies').insert(doc, function(error, result){
+			if (error) {
+				console.log(error);
+				process.exit(1);
+			}
+			var query = { year: 1975, rating : 'PG'}; // comma treated as AND
+			db.collection('movies').find(query).toArray(function(error, docs) {
+				if (error) {
+					console.log(error);
+					process.exit(1);
+				}
+				console.log('Found docs:');
+				docs.forEach(function(doc) {
+					console.log((JSON.stringify(doc)));
+				});
+				process.exit(0);
+			});
 		});
 	});
 });
